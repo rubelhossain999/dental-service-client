c84ec6eac818a916e4c6ad200ab062154d321cc5
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-09 23:46:36 +0600

    Customer Review form design is done with res

--- src/Components/UIDesign/Pages/DetailsService.js
+++ src/Components/UIDesign/Pages/DetailsService.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReviewForm from './ReviewForm';
 
 const DetailsService = () => {
     const serviceDetails = useLoaderData();
     console.log(serviceDetails);
     return (
         <div className='lg:w-9/12 m-auto mt-10 mb-10'>
-            <div className='border-b-1 border-b-orange-300'>
-                <h1 className='flex justify-center items-center text-4xl text-black font-bold'><span className='text-orange-400'>Service Title: </span>{serviceDetails.name}</h1>
+            <div className='border-b-2 border-orange-400'>
+                <h1 className='flex justify-center items-center lg:text-4xl text-black font-bold'><span className='text-orange-400'>Service Title:</span>{serviceDetails.name}</h1>
                 <div className='flex justify-center items-center mt-2'>
                     <img src={serviceDetails.image} alt='service'></img>
                 </div>
                 {/* /<span className='mt-2 text-black'>Post Time: 12: 39 PM</span> */}
-                <p className='m-auto text-black lg:w-3/5 mt-5'>{serviceDetails.description}</p>
+                <p className='m-auto p-5 text-black lg:w-3/5 mt-5'>{serviceDetails.description}</p>
                 <div className='flex justify-center items-center py-6 '>
-                    <button className='btn bg-orange-400 border-none text-black hover:bg-slate-400'>Order Now</button>
+                    <button className='btn bg-orange-400 border-none text-white hover:bg-slate-400'>Order Now</button>
                     <p className='text-2xl px-10 text-black font-bold'>Price: {serviceDetails.price}</p>
                 </div>
+            </div>
+            <div className='mt-5'>
+                <h3 className='font-bold text-2xl text-black text-center'>Customer Review Form</h3>
+                <ReviewForm></ReviewForm>
             </div>
         </div>
     );
